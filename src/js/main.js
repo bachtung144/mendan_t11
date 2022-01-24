86a29a68f4501c532b9b61a7a8c2e0fc013fca2c
bachtung144 <bachtung144007@gmail.com>
2022-01-24 09:43:33 +0700

    fix text

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,41 @@
-jQuery(document).ready(function($) {
+$(document).ready(function(){
 
+    //smooth scroll
+    $(".fixed-side-navbar a, .primary-button a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, () => window.location.hash = hash);
+            //$(selector).animate({params},speed,callback);
+        }
+    });
+
+    //menubar
     $('body').scrollspy({
         target: '.fixed-side-navbar',
         offset: 200
     });
 
+    //tab-group
     $('.tabgroup > div').hide();
     $('.tabgroup > div:first-of-type').show();
     $('.tabs a').click(function(e){
         e.preventDefault();
-        var $this = $(this),
+        let $this = $(this),
             tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
             others = $this.closest('li').siblings().children('a'),
             target = $this.attr('href');
+
+        //handle li tag
         others.removeClass('active');
         $this.addClass('active');
+
+        //handle div tag
         $(tabgroup).children('div').hide();
         $(target).show();
-
     })
 });
+
